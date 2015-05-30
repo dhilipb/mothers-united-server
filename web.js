27c7fe6a2aa136dff3ef14385b3767e5157f898f
Kaushik Chaubal <kaushikchaubal@gmail.com>
2015-05-30 15:12:15 +0100

    Mongo take 2

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
-var express = require('express')
-, cors = require('cors')
-, app = express();
+var express = require('express'),
+    cors = require('cors'),
+    app = express();
 
 var mongojs = require('mongojs');
 var connectionString = process.env.MONGOLAB_URI;
@@ -14,11 +14,11 @@ app.set('port', (process.env.PORT || 3000));
 var globalDB;
 
 function setDB(db) {
-	globalDB = db;
+    globalDB = db;
 };
 
 function getDB() {
-	return globalDB;
+    return globalDB;
 };
 
 var publicQuestions = [];
@@ -45,23 +45,23 @@ var privateQuestions = [];
 */
 
 var testObj = {
-	id: 1,
-	isPublic: true,
-	title: "Title",
-	fbId: "123141",
-	time: "epoch or wathever",
-	name: "Mommy",
-	pregnancyMonth: 6
+    id: 1,
+    isPublic: true,
+    title: "Title",
+    fbId: "123141",
+    time: "epoch or wathever",
+    name: "Mommy",
+    pregnancyMonth: 6
 };
 
 var testObj2 = {
-	id: 2,
-	isPublic: false,
-	title: "New Title",
-	fbId: "123141",
-	time: "epoch or wathever",
-	name: "Mommy",
-	pregnancyMonth: 6
+    id: 2,
+    isPublic: false,
+    title: "New Title",
+    fbId: "123141",
+    time: "epoch or wathever",
+    name: "Mommy",
+    pregnancyMonth: 6
 };
 
 addQuestion(testObj, 1);
@@ -72,67 +72,77 @@ addQuestion(testObj2, 1);
 /* Questions Retrieval */
 app.get('/getPublicQuestions', function(req, res) {
 
-	
-	res.contentType('application/json');
-	res.send(JSON.stringify(returnQuestionList(1)));
+
+    res.contentType('application/json');
+    res.send(JSON.stringify(returnQuestionList(1)));
 });
 
 app.get('/getPrivateQuestions', function(req, res) {
 
 });
 
-app.get('/returnYesIds', function (req, res) {
+app.get('/returnYesIds', function(req, res) {
 
 });
 
-app.get('/returnNoIds', function (req, res) {
+app.get('/returnNoIds', function(req, res) {
 
 });
 
 /* Setter functions */
 function addQuestion(question, status) {
-	if (status) {
-		console.log(db);
-		db.questions.save({created:'just now'});
-		console.log("Getting from DB" + db.questions._get());
-		publicQuestions.push(question);
-	} else {
-		privateQuestions.push(question);
-	}
+    if (status) {
+        console.log(db);
+        db.questions.save({
+            created: 'just now'
+        });
+        db.questions.find(function(err, docs) {
+            console.log("From DB: " + docs);
+        });
+        publicQuestions.push(question);
+    } else {
+        privateQuestions.push(question);
+    }
 }
 
 /* Getter functons */
 function returnQuestionList(status) {
-	if (status) {
-		return publicQuestions;
-	} else {
-		return privateQuestions;
-	}
+    if (status) {
+        return publicQuestions;
+    } else {
+        return privateQuestions;
+    }
 }
 
 
 
-app.get('/getName', function(req, res){
-	console.log("Received request");
-	var repsonseJson = {
-		"http://www.google.com":{"name":"FirstCandidate"},
-		"http://www.facebook.com":{"name":"SecondCandidate"},
-		"http://www.twitter.com":{"name":"ThirdCandidate"}
-	};
-	res.json(repsonseJson);
+app.get('/getName', function(req, res) {
+    console.log("Received request");
+    var repsonseJson = {
+        "http://www.google.com": {
+            "name": "FirstCandidate"
+        },
+        "http://www.facebook.com": {
+            "name": "SecondCandidate"
+        },
+        "http://www.twitter.com": {
+            "name": "ThirdCandidate"
+        }
+    };
+    res.json(repsonseJson);
 });
 
-app.get('/getData', function(req, res){
-	console.log("Received request");
-	var repsonseJson = {
-		"Description":"Legend explaining the graph",
-		"First value":1000,
-		"Second value":2000,
-		"Third value":3000,
-		"Forth value":4000,
-		"Fifth value":5000
-	};
-	res.json(repsonseJson);
+app.get('/getData', function(req, res) {
+    console.log("Received request");
+    var repsonseJson = {
+        "Description": "Legend explaining the graph",
+        "First value": 1000,
+        "Second value": 2000,
+        "Third value": 3000,
+        "Forth value": 4000,
+        "Fifth value": 5000
+    };
+    res.json(repsonseJson);
 });
 
 
@@ -141,16 +151,16 @@ var server = app.listen(app.get('port'), function() {
 });
 
 var totalVote = 0;
-app.post('/vote', function(req, res){
-	console.log("Received request for vote");
-	var name = req.body.name;
-	var newVote = parseInt(req.body.vote);
-	totalVote =  totalVote+newVote;
+app.post('/vote', function(req, res) {
+    console.log("Received request for vote");
+    var name = req.body.name;
+    var newVote = parseInt(req.body.vote);
+    totalVote = totalVote + newVote;
 
 
-	var repsonseJson = {
-		'name' : name,
-		'totalVote' : totalVote
-	};
-	res.json(repsonseJson);
+    var repsonseJson = {
+        'name': name,
+        'totalVote': totalVote
+    };
+    res.json(repsonseJson);
 });
