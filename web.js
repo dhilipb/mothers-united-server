23d6ab83e52a0d58f866d1362780183d36e2b9b1
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2015-05-30 20:03:53 +0100

    Remove isPublic

--- web.js
+++ web.js
@@ -17,21 +17,18 @@ var databaseArrays = [
 var db = mongojs(connectionString, databaseArrays);
 
 /* Questions Retrieval */
-app.get('/getPublicQuestions', function(req, res) {
-
-    res.contentType('application/json');
-    db.publicQuestions.find(function(err, docs) {
-        console.log("Retrieved from publicQuestions and sending: ", docs);
-        res.send(docs);
-    });
-});
-
-app.get('/getPrivateQuestions', function(req, res) {
-    res.contentType('application/json');
+app.get('/questions', function(req, res) {
+  res.contentType('application/json');
+  var fbId = req.param('facebookId');
+  if (fbId) {
     db.privateQuestions.find(function(err, docs) {
-        console.log("Retrieved from privateQuestions and sending: ", docs);
-        res.send(docs);
+      res.send(docs);
     });
+  } else {
+    db.publicQuestions.find(function (err, docs) {
+      res.send(docs);
+    });
+  }
 });
 
 app.get('/returnYesIds', function(req, res) {
@@ -50,7 +47,7 @@ app.get('/returnNoIds', function(req, res) {
 
 });
 
-app.post('/addQuestion', function(req, res) {
+app.post('/questions/new', function(req, res) {
     console.log("Received request for addQuestion");
     var isPublic = req.body.facebookId;
 
@@ -68,12 +65,7 @@ app.post('/yes', function(req, res) {
     db.publicQuestions.findOne({
       _id:mongojs.ObjectId(questionId)
     }, function (err, doc) {
-      var user = {
-        name: "Vlad",
-        type: "doctor"
-      };
-      var listOfUsers = [];
-      listOfUsers.push(user);
+      // List of users or count
 
       doc.users = listOfUsers;
       res.send(doc.users);
@@ -83,16 +75,7 @@ app.post('/yes', function(req, res) {
 });
 
 app.post('/no', function(req, res) {
-    console.log("Received request for addQuestion");
-    var isPublic = req.body.facebookId;
 
-    if (isPublic) {
-        db.publicQuestions.save(req.body);
-    } else {
-        db.privateQuestions.save(req.body);
-    }
-
-    res.json(req.body);
 });
 
 
