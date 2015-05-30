65ecb9784b805eb06ff39483546dc60b9075025a
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2015-05-30 22:44:49 +0100

    Testing

--- web.js
+++ web.js
@@ -63,7 +63,7 @@ app.post('/questions/vote', function (req, res) {
   } else {
     console.log("false branch");
     db.questions.update(
-      { _id: qId },
+      { creatorAge: 25 },
       { $set: {title: "object"} }
     );
   }
